a1d95ab7fc8019cc1be100399db7f76d3e848166
Daniel Jones <daniel@poweredbybees.com>
2014-09-22 20:33:18 +0700

    Slight change to test

--- test/default.js
+++ test/default.js
@@ -4,6 +4,9 @@ var assert = require('assert');
     describe('The way it smells', function () {
         it('is pungent', function(){
             assert.ok(true);
+        });
+        it('is foul',function(){
+            assert.ok(true);
         })
 
     });
